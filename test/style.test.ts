91aafc4f7d82ae439468500164cfbdb26c773ed6
Andrew Prasetya <andrew@donkeymob.local>
2020-02-03 11:49:24 -0500

    Added small tests.

--- test/style.test.ts
+++ test/style.test.ts
@@ -1,19 +1,22 @@
 import '@testing-library/jest-dom';
 
 import {
-  ascendFrom,
+  getTotalHorizontalMargins,
+  getTotalVerticalMargins,
 } from '../lib/doko';
 
 describe('test DOM stuff', () => {
   document.body.innerHTML = `
     <div id="root">
-      <div id="node">
+      <div id="node" style="margin: 20px;">
       </div>
     </div>
   `;
 
   const node = document.getElementById('node');
-  it('should return not null', () => {
-    expect(ascendFrom(node as HTMLElement, element => true));
+
+  it('should return the corrent vertical and horizontal margins.', () => {
+    expect(getTotalHorizontalMargins(node as HTMLElement)).toEqual(40);
+    expect(getTotalVerticalMargins(node as HTMLElement)).toEqual(40);
   });
 });
